595d870b4dfc0882235209a3f8250a99a23d35b9
jtao <1817731534@qq.com>
2021-08-13 14:45:59 +0800

    first commit

---
+++ src/test/java/com/jtcode/AppTest.java
@@ -0,0 +1,20 @@
+package com.jtcode;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
